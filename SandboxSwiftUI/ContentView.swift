96556f2d9222968cd0e4c70b00fe048b4294c522
Ana Raiany <ana.guimaraes63@akuno.ifce.edu.br>
2022-05-11 14:55:16 -0300

    Initial Commit

---
+++ SandboxSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SandboxSwiftUI
+//
+//  Created by Ana Raiany Guimarães Gomes on 11/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SandboxSwiftUI/SandboxSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandboxSwiftUIApp.swift
+//  SandboxSwiftUI
+//
+//  Created by Ana Raiany Guimarães Gomes on 11/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SandboxSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
